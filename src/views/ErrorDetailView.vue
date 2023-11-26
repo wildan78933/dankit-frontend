bca8931d8a83f9a5e775325f54e8c9799e94ac4e
wildan <wildan78933@gmail.com>
2023-11-26 18:11:22 +0700

    update

---
+++ src/views/ErrorDetailView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative overflow-hidden bg-white">
+    <div class="mx-auto max-w-7xl">
+      <div
+        class="flex flex-col items-center w-full min-h-screen pt-6 sm:justify-center sm:pt-0"
+      >
+        <div class="w-full p-5 mx-auto sm:max-w-md">
+          <img
+            src="@/assets/img/success-illustration.svg"
+            alt=""
+            class="mb-10 w-4xl"
+          />
+          <h2 class="mb-2 text-2xl font-bold text-center">
+            You have to register or login first
+          </h2>
+          <p class="text-gray-500 text-center">
+            Sorry, but to be able to see the product detail page, you must log
+            in or register first
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
